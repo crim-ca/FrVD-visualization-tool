263f01ecbdc0443c1e45beec8f4822b6abe63069
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-11-12 20:02:36 -0500

    add initial parsing/update of metadata

--- viewer.py
+++ viewer.py
@@ -53,25 +53,37 @@ class VideoResultPlayerApp(object):
     fps = None
     frame = None
     frame_time = 0
-    frame_last = 0
     frame_index = None
     frame_count = None
     frame_output = None
+    last_time = 0
     # metadata references
     video_annot_meta = None
+    video_annot_index = None
     video_infer_meta = None
+    video_infer_index = None
     text_infer_meta = None
+    text_infer_index = None
     # handles to UI elements
     window = None
     video_viewer = None
     video_annot_label = None
     video_annot_scroll = None
     video_annot_textbox = None
+    video_infer_label = None
+    video_infer_scroll = None
+    video_infer_textbox = None
+    text_infer_label = None
+    text_infer_scroll = None
+    text_infer_textbox = None
     snapshot_button = None
     play_button = None
     play_state = True
     play_label = None
     play_text = None
+    font_header = ("Arial", 16, "bold")
+    font_code = ("Courier", 12, "normal")
+    font_normal = ("Arial", 12, "normal")
 
     def __init__(self, video_file, video_annotations, video_results, text_results, output=None):
         self.video_source = os.path.abspath(video_file)
@@ -83,13 +95,13 @@ class VideoResultPlayerApp(object):
         self.frame_output = output
 
         self.setup_player()
-        self.setup_metadata(video_annotations, video_results, text_results)
         self.setup_window()
+        if not self.setup_metadata(video_annotations, video_results, text_results):
+            return
 
         self.run()
 
     def run(self):
-        self.update()
         self.update_video()  # after called once, update method will call itself with delay to loop frames
         self.window.mainloop()  # blocking
         LOGGER.log(logging.INFO if self.error else logging.ERROR, "Exit")
@@ -114,33 +126,52 @@ class VideoResultPlayerApp(object):
         self.window.title("Result Viewer")
         # Create a canvas that can fit the above video source size
         self.video_viewer = tk.Canvas(self.window, width=self.video_width, height=self.video_height)
-        self.video_viewer.pack()
+        self.video_viewer.pack(anchor=tk.NW)
 
-        # Button that lets the user take a snapshot
+        self.video_annot_label = tk.Label(self.window, text="Video Annotation Metadata", font=self.font_header)
+        self.video_annot_label.pack(side=tk.LEFT)
+        self.video_annot_textbox = tk.Text(self.window, height=20, width=50)
+        self.video_annot_scroll = tk.Scrollbar(self.window, command=self.video_annot_textbox.yview)
+        self.video_annot_textbox.configure(yscrollcommand=self.video_annot_scroll.set)
+        self.video_annot_textbox.tag_configure("big", font=self.font_header)
+        self.video_annot_textbox.tag_configure("code", font=self.font_code)
+        self.video_annot_textbox.tag_configure("normal", foreground='#476042', font=self.font_normal)
+        self.video_annot_textbox.insert(tk.END, "<nodata>", "code")
+        self.video_annot_textbox.pack(side=tk.LEFT)
+        self.video_annot_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.video_infer_label = tk.Label(self.window, text="Video Inference Metadata", font=self.font_header)
+        self.video_infer_label.pack(side=tk.LEFT)
+        self.video_infer_textbox = tk.Text(self.window, height=20, width=50)
+        self.video_infer_scroll = tk.Scrollbar(self.window, command=self.video_infer_textbox.yview)
+        self.video_infer_textbox.configure(yscrollcommand=self.video_infer_scroll.set)
+        self.video_infer_textbox.tag_configure("big", font=self.font_header)
+        self.video_infer_textbox.tag_configure("code", font=self.font_code)
+        self.video_infer_textbox.tag_configure("normal", foreground='#476042', font=self.font_normal)
+        self.video_infer_textbox.insert(tk.END, "<nodata>", "code")
+        self.video_infer_textbox.pack(side=tk.LEFT)
+        self.video_infer_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.text_infer_label = tk.Label(self.window, text="Text Inference Metadata", font=self.font_header)
+        self.text_infer_label.pack(side=tk.LEFT)
+        self.text_infer_textbox = tk.Text(self.window, height=20, width=50)
+        self.text_infer_scroll = tk.Scrollbar(self.window, command=self.text_infer_textbox.yview)
+        self.text_infer_textbox.configure(yscrollcommand=self.text_infer_scroll.set)
+        self.text_infer_textbox.tag_configure("big", font=self.font_header)
+        self.text_infer_textbox.tag_configure("code", font=self.font_code)
+        self.text_infer_textbox.tag_configure("normal", foreground="#476042", font=self.font_normal)
+        self.text_infer_textbox.insert(tk.END, "<nodata>", "code")
+        self.text_infer_textbox.pack(side=tk.LEFT)
+        self.text_infer_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
         self.snapshot_button = tk.Button(self.window, text="Snapshot", width=50, command=self.snapshot)
-        self.snapshot_button.pack(anchor=tk.E, expand=True)
+        self.snapshot_button.pack(side=tk.LEFT)
 
         self.play_state = True
         self.play_text = tk.StringVar()
         self.play_text.set("PAUSE")
         self.play_button = tk.Button(self.window, textvariable=self.play_text, width=40, command=self.toggle_playing)
-        self.play_button.pack(side=tk.RIGHT)
-
-        self.video_annot_label = tk.Label(self.window, textvariable="Video Annotation Metadata\n")
-        self.video_annot_label.pack(side=tk.LEFT)
-        #self.video_annot_label.tag_configure("big", font=("Arial", 20, "bold"))
-        self.video_annot_textbox = tk.Text(self.window, height=20, width=50)
-        self.video_annot_scroll = tk.Scrollbar(self.window, command=self.video_annot_textbox.yview)
-        self.video_annot_textbox.configure(yscrollcommand=self.video_annot_scroll.set)
-        self.video_annot_textbox.tag_configure("big", font=("Arial", 20, "bold"))
-        self.video_annot_textbox.tag_configure("code", font=("Courier", 12, "normal"))
-        self.video_annot_textbox.tag_configure("normal", foreground='#476042', font=("Arial", 12, "bold"))
-        #self.video_annot_textbox.tag_bind("follow", "<1>", lambda e, t=self.video_annot_textbox: t.insert(tk.END, "event"))
-        #self.video_annot_textbox.insert(tk.END, "Video Annotation Metadata\n", "big")
-        #self.video_annot_textbox.insert(tk.END, textvariable=self.video_annotation, "normal")
-        self.video_annot_textbox.insert(tk.END, "<nodata>", "code")
-        self.video_annot_textbox.pack(side=tk.LEFT)
-        self.video_annot_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        self.play_button.pack(side=tk.LEFT)
 
     def toggle_playing(self):
         if self.play_state:
@@ -151,12 +182,48 @@ class VideoResultPlayerApp(object):
             LOGGER.debug("Video resume.")
         self.play_state = not self.play_state
 
-    def update(self):
-        self.window.after(10, self.update)
-
-    def update_text(self, text):
+    def update_annotation(self, metadata):
+        text = metadata["vd"]
         self.video_annot_textbox.delete("1.0", tk.END)
         self.video_annot_textbox.insert(tk.END, text, "normal")
+
+    def update_infer_video(self, metadata):
+        header = "[Score] [Classes] (ordered)\n\n  "
+        values = "\n  ".join(["[{:.2f}]  {})".format(s, c) for c, s in zip(metadata["classes"], metadata["scores"])])
+        text = header + values
+        self.video_infer_textbox.delete("1.0", tk.END)
+        self.video_infer_textbox.insert(tk.END, text, "normal")
+
+    def update_infer_text(self, metadata):
+        text = json.dumps(metadata, indent=2, ensure_ascii=False)
+        self.text_infer_textbox.delete("1.0", tk.END)
+        self.text_infer_textbox.insert(tk.END, text, "normal")
+
+    def update_metadata(self):
+        if self.video_annot_meta:
+            current_meta = self.video_annot_meta[self.video_annot_index]
+            if self.frame_time > current_meta["end_ms"]:
+                if self.video_annot_index < len(self.video_annot_meta) - 1:
+                    self.video_annot_index += 1
+                    self.update_annotation(self.video_annot_meta[self.video_annot_index])
+                else:
+                    self.update_annotation(None)
+        if self.video_infer_meta:
+            current_meta = self.video_infer_meta[self.video_infer_index]
+            if self.frame_time > current_meta["end_ms"]:
+                if self.video_infer_index < len(self.video_infer_meta) - 1:
+                    self.video_infer_index += 1
+                    self.update_infer_video(self.video_infer_meta[self.video_infer_index])
+                else:
+                    self.update_infer_video(None)
+        if self.text_infer_meta:
+            current_meta = self.text_infer_meta[self.text_infer_index]
+            if self.frame_time > current_meta["end_ms"]:
+                if self.text_infer_index < len(self.text_infer_meta) - 1:
+                    self.text_infer_index += 1
+                    self.update_infer_text(self.text_infer_meta[self.text_infer_index])
+                else:
+                    self.update_infer_text(None)
 
     def update_video(self):
         """
@@ -167,7 +234,7 @@ class VideoResultPlayerApp(object):
             self.window.after(30, self.update_video)
             return
 
-        LOGGER.debug("Frame: %s, Real FPS: %s", self.frame_index, self.frame_time - self.frame_last)
+        LOGGER.debug("Frame: %s, Real FPS: %s", self.frame_index, self.frame_time - self.last_time)
         ret, frame = self.video.read()
         if not ret:
             LOGGER.error("Playback error occurred when reading next video frame.")
@@ -175,8 +242,9 @@ class VideoResultPlayerApp(object):
             return
 
         self.frame_index = int(self.video.get(cv.CAP_PROP_POS_FRAMES))
-        self.frame_last = self.frame_time
+        self.last_time = self.frame_time
         self.frame_time = self.video.get(cv.CAP_PROP_POS_MSEC)
+        self.update_metadata()
 
         # display basic information
         text_position = (10, 25)
@@ -197,16 +265,27 @@ class VideoResultPlayerApp(object):
         self.window.after(math.floor(self.frame_delta), self.update_video)
 
     def setup_metadata(self, video_annotations, video_results, text_results):
-        # parse available metadata files
-        if video_annotations and os.path.isfile(video_annotations):
-            LOGGER.info("Parsing video inference metadata [%s]...", video_annotations)
-            self.video_annot_meta = self.parse_video_annotation_metadata(video_annotations)
-        if video_results and os.path.isfile(video_results):
-            LOGGER.info("Parsing video inference metadata [%s]...", video_results)
-            self.video_infer_meta = self.parse_video_inference_metadata(video_results)
-        if text_results and os.path.isfile(text_results):
-            LOGGER.info("Parsing text inference metadata [%s]...", text_results)
-            self.text_infer_meta = self.parse_text_inference_metadata(text_results)
+        """
+        Parse available metadata files and prepare the first entry according to provided file references.
+        """
+        try:
+            if video_annotations and os.path.isfile(video_annotations):
+                LOGGER.info("Parsing video description metadata [%s]...", video_annotations)
+                self.video_annot_meta = self.parse_video_annotation_metadata(video_annotations)
+                self.video_annot_index = 0
+            if video_results and os.path.isfile(video_results):
+                LOGGER.info("Parsing video inference metadata [%s]...", video_results)
+                self.video_infer_meta = self.parse_video_inference_metadata(video_results)
+                self.video_infer_index = 0
+            if text_results and os.path.isfile(text_results):
+                LOGGER.info("Parsing text inference metadata [%s]...", text_results)
+                self.text_infer_meta = self.parse_text_inference_metadata(text_results)
+                self.text_infer_index = 0
+        except Exception as exc:
+            self.error = True
+            LOGGER.error("Invalid formats. One or more metadata file could not be parsed.", exc_info=exc)
+            return False
+        return True
 
     def parse_video_annotation_metadata(self, metadata_path):
         try:
@@ -221,36 +300,41 @@ class VideoResultPlayerApp(object):
             episode_str = " - {}".format(episode) if episode else ""
             self.video_title = "[{}] {}{}".format(collection, title, episode_str)
 
-            return metadata
+            meta_vd = metadata.get("augmented_vd_metadata")
+            if meta_vd:
+                LOGGER.info("Retrieved augmented video-description metadata.")
+            else:
+                meta_vd = metadata.get("standard_vd_metadata")
+                if meta_vd:
+                    LOGGER.info("Retrieved standard video-description metadata (augmented not provided).")
+            if meta_vd:
+                # ensure sorted entries
+                return list(sorted(meta_vd, key=lambda vd: vd["start_ms"]))
         except Exception as exc:
             LOGGER.error("Could not parse video annotation metadata file: [%s]", metadata_path, exc_info=exc)
         return None
 
-    def parse_video_inference_metadata(self, metadata_path):
+    @staticmethod
+    def parse_video_inference_metadata(metadata_path):
         try:
             with open(metadata_path) as meta_file:
                 metadata = json.load(meta_file)
             # ensure ordered by time
-            metadata["predictions"] = list(sorted(metadata["predictions"], key=lambda p: p["start"]))
-
-            # ease retrieval of active prediction against current video frame
-            cumul_ms = 0.
-            delta_ms = self.frame_delta
-            total = delta_ms
-            frame = 0
-            for pred in metadata["predictions"]:
-                if pred["start"] > delta_ms:
-                    frame += 1
-                pred["frame"] = frame
-                cumul_ms += pred["end"]
-            return metadata
+            predictions = list(sorted(metadata["predictions"], key=lambda p: p["start"]))
+            # convert times to ms for same base comparisons
+            for pred in predictions:
+                pred["start_ms"] = pred["start"] * 1000
+                pred["end_ms"] = pred["end"] * 1000
+            return predictions
         except Exception as exc:
             LOGGER.error("Could not parse video inference metadata file: [%s]", metadata_path, exc_info=exc)
         return None
 
-    def parse_text_inference_metadata(self, metadata_path):
+    @staticmethod
+    def parse_text_inference_metadata(metadata_path):
         try:
             pass
+            # TODO: convert TS: [start,end] -> start_ms, end_ms
         except Exception as exc:
             LOGGER.error("Could not parse text inference metadata file: [%s]", metadata_path, exc_info=exc)
         return None
@@ -278,9 +362,9 @@ class VideoResultPlayerApp(object):
 def make_parser():
     ap = argparse.ArgumentParser(prog=__NAME__, description=__doc__, add_help=True)
     ap.add_argument("video_file", help="Video file to view.")
-    ap.add_argument("video_annotations", help="File with original video-description annotations.")
-    ap.add_argument("video_results", help="File with video actions inference results.")
-    ap.add_argument("text_results", help="File with text subjects and verbs results.")
+    ap.add_argument("video_annotations", help="JSON metadata file with original video-description annotations.")
+    ap.add_argument("video_results", help="JSON metadata file with video actions inference results.")
+    ap.add_argument("text_results", help="JSON metadata file with text subjects and verbs results.")
     ap.add_argument("--output", "-o", help="Output location of frame snapshots.", default="/tmp/video-result-viewer")
     ap.add_argument("--debug", "-d", action="store_true", help="Debug logging.")
     return ap
